d26fd7c77782b0466a04b6a3539485f68cd6773d
SHollizeck <sebastian.hollizeck@gmx.de>
2019-09-17 12:04:58 +1000

    added human lesion site plotting
    The functions to colour sites in a human body have been added, I also sneaked the first attempt at my varaint parsing in there

---
+++ R/humanRegionPlotting.R
@@ -0,0 +1,156 @@
+
+
+#' Change values for plotting
+#'
+#' @param mappingXML xml representing the picture
+#' @param organ which organ to change
+#' @param style which style (fill/stroke)
+#' @param color which color to plot this organ in
+#' @param lwd line width to use for stroke style
+#'
+#' @return TRUE if something got change FALSE otherwise
+#' @export
+#'
+#' @import XML
+#'
+#' @examples
+changePlottingStyle <- function(mappingXML,organ,style = "fill" ,color="black",lwd="5"){
+
+  availableOrgans <- getAllOrgans(mappingXML)
+
+  if( ! organ %in% availableOrgans ){
+
+    warning("Could not find the specified organ: ",organ,", but will continue")
+    return(invisible(FALSE))
+  }
+  parts <- strsplit(organ,"_")[[1]]
+  parent <- ""
+  if(length(parts) == 3){
+    parent <- paste0(parts[c(1,3)],collapse ="_")
+
+
+  }else if( length(parts) == 2){
+    parent <- parts[2]
+  }
+  if(parent %in% availableOrgans ){
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",parent,"']",sep=""))[[1]]
+    XML::xmlAttrs(node)["type"]<-"stroke"
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",parent,"']/context/style",sep=""))[[1]]
+    XML::xmlAttrs(node)["lwd"]<-1
+  }
+
+  # start with the style (either fill or stroke or invis)
+  if ( style == "fill" ){
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']",sep=""))[[1]]
+    XML::xmlAttrs(node)["type"]<-"fill"
+  }else if ( style == "stroke" ){
+    #this should be the default, but you never know
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']",sep=""))[[1]]
+    XML::xmlAttrs(node)["type"]<-"stroke"
+  }else if (style == "invis" ){
+    # this is a small workaround, by setting the line width to 0
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']",sep=""))[[1]]
+    XML::xmlAttrs(node)["type"]<-"stroke"
+
+    node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']/context/style",sep=""))[[1]]
+    XML::xmlAttrs(node)["lwd"]<-"0"
+    return(invisible(TRUE))
+
+  }
+
+  #then go for the color
+  node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']/context/rgb",sep=""))[[1]]
+  # dissassemble the color into rgb and then normalize to [0,1]
+  rgbCol <- grDevices::col2rgb(color)
+  XML::xmlAttrs(node)["r"]=rgbCol[1]/255
+  XML::xmlAttrs(node)["g"]=rgbCol[2]/255
+  XML::xmlAttrs(node)["b"]=rgbCol[3]/255
+
+  #last we go for the line width, but only if the style is stroke
+  node<-XML::xpathSApply(mappingXML, paste("//path[@id='",organ,"']/context/style",sep=""))[[1]]
+  if ( style == "stroke" ){
+    XML::xmlAttrs(node)["lwd"]<-lwd
+  }else{
+    XML::xmlAttrs(node)["lwd"]<-1
+  }
+}
+
+#' Get all available organs
+#'
+#' this extracts the top level ids of each child of the root xml
+#'
+#' @param mappingXML the xml representing the picture
+#'
+#' @return list of all organs to chose from
+#'
+#' @export
+#'
+#' @examples
+getAllOrgans <- function(mappingXML){
+
+  root <- XML::xmlRoot(mappingXML)
+
+  children <- XML::xmlChildren(root)
+
+  organs <- sapply(children, function(x) XML::xmlAttrs(x)[2])
+
+  #as the last entry is the one which sums all the others, we do not need the last one
+  organs <- organs[-length(organs)]
+  return(as.vector(organs))
+}
+
+
+#' Create XML object from file
+#'
+#' This is mainly used to load the XML representing the human anatomy for plotting purposes
+#' can be used for other XMLs as well
+#'
+#' @param file to be converted into an object
+#'
+#' @return the XML tree object
+#' @export
+#'
+#' @examples
+#' #loading the internal xml for plotting
+#' mappingXML <- loadXML()
+#'
+loadXML <- function(file=system.file("exdata", "human_anatomy.ps.xml", package="SHollizeckPackage")){
+  #check if the file exists and exit otheriwse
+  if(!file.exists(file)){
+    stop("Specified file does not exist ", file)
+  }
+
+  obj <- XML::xmlParse(file)
+
+  return(obj)
+}
+
+
+#' Plot XMLObject
+#'
+#' This function converts the XML into a plotable grid object
+#'
+#' @param mappingXML the XML with the specs to plot
+#' @param return where to plot (\code{FALSE}) or return (\code{TRUE}) the object
+#'
+#' @return the object if return is set to \code{TRUE}
+#' @export
+#'
+#' @seealso loadXML,changeStyle
+#' @examples
+#' #draw the human anatomy
+#' drawXML(loadXML())
+#'
+drawXML <- function(mappingXML, return=FALSE){
+
+  #convert XML to picture object
+  pic <- grImport::readPicture(XML::saveXML(mappingXML))
+  if(return){
+    #return the object instead
+    return(pic)
+  }else{
+    #plot picture
+    grImport::grid.picture(pic)
+  }
+
+}
---
+++ R/vcfparsing.R
@@ -0,0 +1,135 @@
+
+
+
+#' Reads structural variants vcf
+#'
+#' Turns the vcf into an annotated data.table
+#'
+#' @param file the vcf file to be read in
+#' @param minlength minimum length of a variant to be considered
+#' @param chrs which chromosomes to restrict the variants on
+#' @param gns which genes to use to annotate
+#' @param biomaRtDB which biomart db to use
+#'
+#' @import BiocGenerics
+#' @import GenomeInfoDb
+#' @import TxDb.Hsapiens.UCSC.hg19.knownGene
+#' @import org.Hs.eg.db
+#' @import GenomicFeatures
+#'
+#' @import biomaRt
+#' @rawNamespace import(data.table, except = "shift")
+#' @import GenomicRanges
+#'
+#' @return data.table with annotated structural variants
+#' @export
+#'
+#' @examples
+#' #minimal example here
+getStructuralVariants <- function(file, minlength=10000,chrs=c(1:22, "X", "Y"),gns,biomaRtDB) {
+
+  sv_vcf <-VariantAnnotation::readVcf(file,"hg19")
+
+  # get the genes in the ranges
+  sv_gr <- StructuralVariantAnnotation::breakpointRanges(sv_vcf)
+
+  #remove small variants on the same chromosome
+  sv_gr <- sv_gr[seqnames(sv_gr) != seqnames(partner(sv_gr)) | abs(start(sv_gr) - start(StructuralVariantAnnotation::partner(sv_gr))) > minlength]
+
+  #for biomarot we need the chr in the front
+  seqlevelsStyle(sv_gr) <- "UCSC"
+
+  #check if the supplied chrs already have UCSC style, otherwise add the chr
+  if( ! all(grepl("^chr",chrs))){
+    chrs<- paste0("chr",chrs)
+  }
+
+  # remove non canonical variants
+  sv_gr_can <- sv_gr[seqnames(sv_gr) %in% chrs]
+  # now remove entries that had partners there
+  sv_gr_can <- sv_gr_can[sv_gr_can$partner %in% names(sv_gr_can)]
+
+  seqlevels(sv_gr_can) <- chrs
+
+  # if there were no genes supplied, we get our own
+  if (missing(gns)){
+    gns <- GenomicFeatures::genes(TxDb.Hsapiens.UCSC.hg19.knownGene)
+  }else{
+    # do some test to see if the arugment makes sense
+    if( class(gns)[1] != "GRanges"){
+      warning("This does not look like a proper biomaRtDB")
+      #do we exit here or wait for biomaRt to throw the error?
+    }
+  }
+  # same goes for the database
+  if(missing(biomaRtDB)){
+    biomaRtDB <- org.Hs.eg.db
+  }else{
+    # do some test to see if the arugment makes sense
+    if( class(biomaRtDB)[1] != "OrgDb"){
+      warning("This does not look like a proper biomaRtDB")
+      #do we exit here or wait for biomaRt to throw the error?
+    }
+  }
+
+  hits <- data.table::as.data.table(GenomicRanges::findOverlaps(sv_gr_can, gns, ignore.strand=TRUE))
+  hits$SYMBOL <- biomaRt::select(biomaRtDB, gns[hits$subjectHits]$gene_id, "SYMBOL")$SYMBOL
+  hits$gene_strand <- as.character(strand(gns[hits$subjectHits]))
+
+  hits[,SYMBOL:=paste(.SD$SYMBOL,collapse = ","), by=list(queryHits)]
+
+  sv_gr_can$SYMBOL <- NA
+  sv_gr_can$SYMBOL[hits$queryHits] <- hits$SYMBOL
+  sv_gr_can$geneStrand <- NA
+  sv_gr_can$geneStrand[hits$queryHits] <- hits$gene_strand
+
+  #annotate the frequency of the variant
+  bafs <-apply(VariantAnnotation::geno(sv_vcf[sv_gr_can$sourceId])$PR,1,function(x) {
+    normal <- x[[1]]
+    tumor <- x[[2]]
+
+    baf1Normal <- normal[1]/sum(normal,na.rm=T)
+    baf2Normal <- normal[2]/sum(normal,na.rm=T)
+
+    baf1Tumor <- tumor[1]/sum(tumor,na.rm=T)
+    baf2Tumor <- tumor[2]/sum(tumor,na.rm=T)
+
+    return(c(baf1Normal,baf2Normal,baf1Tumor,baf2Tumor))
+  })
+
+  #flip them in the right way and add names
+  bafs <- as.data.frame(t(bafs))
+  colnames(bafs) <- c("mafNormal","bafNormal","mafTumor","bafTumor")
+
+  # we do this the ugly way, because otherwise it wont stay a granges object
+  sv_gr_can$mafNormal <- bafs$mafNormal
+  sv_gr_can$bafNormal <- bafs$bafNormal
+  sv_gr_can$mafTumor <- bafs$mafTumor
+  sv_gr_can$bafTumor <- bafs$bafTumor
+
+  #depth of coverage is sometimes a nice info, even though for duplications and similar it is not really a thing
+  sv_gr_can$depth <- VariantAnnotation::info(sv_vcf[sv_gr_can$sourceId])$BND_DEPTH
+
+  # need to convert to dataframe first to keep the rownames
+  sv_dt <- as.data.table(as.data.frame(sv_gr_can),keep.rownames= "ID")
+
+  #self join on the partner to get the info of both break points
+  sv_dt_all <- merge(sv_dt,sv_dt,by.x="ID",by.y="partner")
+
+  # now we remove the duplications
+  mn <- pmin(sv_dt_all$partner, sv_dt_all$ID)
+  mx <- pmax(sv_dt_all$partner, sv_dt_all$ID)
+  int <- as.numeric(interaction(mn, mx))
+  sv_dt_nodup<- sv_dt_all[match(unique(int), int),]
+
+  #set the strand to a usuable format
+  sv_dt_nodup$geneStrand.x <- as.character(sv_dt_nodup$geneStrand.x)
+  sv_dt_nodup$geneStrand.x[is.na(sv_dt_nodup$geneStrand.x)] <- ""
+  sv_dt_nodup$geneStrand.y <- as.character(sv_dt_nodup$geneStrand.y)
+  sv_dt_nodup$geneStrand.y[is.na(sv_dt_nodup$geneStrand.y)] <- ""
+
+  #and do the same with the gene
+  sv_dt_nodup$SYMBOL.x[is.na(sv_dt_nodup$SYMBOL.x)] <- ""
+  sv_dt_nodup$SYMBOL.y[is.na(sv_dt_nodup$SYMBOL.y)] <- ""
+  return(sv_dt_nodup)
+}
